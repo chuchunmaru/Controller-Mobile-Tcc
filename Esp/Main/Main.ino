34b221524cc6b6561be27534df9ab60763ea690a
Chuchunmaru <85150222+chuchunmaru@users.noreply.github.com>
2021-12-08 16:04:49 -0300

    Update Main.ino

--- Esp/Main/Main.ino
+++ Esp/Main/Main.ino
@@ -31,49 +31,5 @@ void setup(){
 
 void loop()
 { 
-  gasMeasurement = map(analogRead(MQ2_S), 0, 4095, 0, 500);
   runInLoop();
-  if(gasMeasurement > 180){
-    leak = true;
-    digitalWrite(LED_G, 0);
-    digitalWrite(LED_Y, 0);
-    digitalWrite(LED_R, 0);
-    if(gasMeasurement > 180 && gasMeasurement < 360){
-      digitalWrite(LED_Y, 1);
-      digitalWrite(BUZZER, 1);
-      servo.write(90);
-      delay(2500);
-      gasMeasurement = map(analogRead(MQ2_S), 0, 4095, 0, 500);
-      if(gasMeasurement < 180){
-        digitalWrite(LED_G, 0);
-        digitalWrite(LED_Y, 0);
-        digitalWrite(LED_R, 0);
-        digitalWrite(LED_Y, 0);
-        digitalWrite(BUZZER, 0);
-      }
-    }
-    else if(gasMeasurement > 180 && gasMeasurement > 360){
-      digitalWrite(LED_G, 0);
-      digitalWrite(LED_Y, 0);
-      digitalWrite(LED_R, 0);
-      digitalWrite(BUZZER, 1);
-      digitalWrite(LED_R, 1);
-      digitalWrite(RELAY, 1);
-      servo.write(90);
-      delay(2500);
-      gasMeasurement = map(analogRead(MQ2_S), 0, 4095, 0, 500);
-    }
-    if(leak && gasMeasurement < 180){
-    digitalWrite(LED_G, 0);
-    digitalWrite(LED_Y, 0);
-    digitalWrite(LED_R, 0);
-    digitalWrite(BUZZER, 0);
-    servo.write(0);
-    delay(2500);
-    leak = false;
-  }
-  }
-  if(!leak){
-    digitalWrite(LED_G, 1);
-  }
 }

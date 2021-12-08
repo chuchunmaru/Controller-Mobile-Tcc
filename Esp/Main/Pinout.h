fcb8d676d22ecfdf468554a9eb85da30dd5ade09
Chuchunmaru <85150222+chuchunmaru@users.noreply.github.com>
2021-12-08 16:05:08 -0300

    Delete Esp directory

--- Esp/Main/Pinout.h
+++
@@ -1,8 +0,0 @@
-const byte BUILTIN_LED = 2;
-const byte MQ2_S = 34; 
-const byte LED_R = 12; 
-const byte LED_G = 13; 
-const byte LED_Y = 14; 
-const byte BUZZER = 27; 
-const byte RELAY = 26; 
-const byte SERVO = 25; 
